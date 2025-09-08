74a718bdacd1d75116c2522cd7ef3e027fd9308d
ShaneRer1 <shane53723@gmail.com>
2025-09-09 00:04:30 +1000

    Added auto date time addition

--- Tracker.py
+++ Tracker.py
@@ -1,6 +1,8 @@
-expenses = [{'date': '23', 'category': '02', 'amount': 23.0, 'description': 'fresh'}, {'date': '23-25-02', 'category': 'sfsd', 'amount': 1236.0, 'description': 'frd'}]
+from datetime import datetime   
+expenses = [{'date': '08-09-2025 23:57:42', 'category': 'Groceries', 'amount': 87.99, 'description': 'Grocery shop at coles'}, 
+            {'date': '08-09-2025 23:58:33', 'category': 'Travel', 'amount': 12.65, 'description': 'Uber to work'}]
 while True:
-    print("Track your stuff boss\n" \
+    print("\nTrack your stuff boss\n" \
     "Options:\n" \
     "1. Add expense\n" \
     "2. View expenses\n" \
@@ -9,16 +11,19 @@ while True:
     choice = input("Enter Choice: ")
 
     if choice == '1':
-        date = input("Enter date (YY-MM-DD): ")
+        now = datetime.now()
+        date = now.strftime("%d-%m-%Y %H:%M:%S")
         category = input("Enter Category: ")
         amount = float(input("Enter Amount: "))
         description = input("Enter short description: ")
         expenses.append({"date": date, "category": category, "amount": amount, "description": description})
 
     elif choice == '2':
-        print(f"{'Date':<10}\t{'Category':<10}\t{'Amount':<10}\t{'Description':<10}")
+        print(f"{'Date':<20} | {'Category':<10} | {'Amount':<10} | {'Description':<30}")
+        print("-" * 80)
         for expense in expenses:
-            print(f"{expense['date']:<10} | {expense['category']:<10} | ${expense['amount']:<10} | {expense['description']:<10}")
+            print(f"{expense['date']:<20} | {expense['category']:<10} | ${expense['amount']:<10.2f} | {expense['description']:<30}")
+            #print(expense)
 
     elif choice == '3':
         total = sum(expense['amount'] for expense in expenses)
