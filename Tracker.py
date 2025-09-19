573dc05025996e045ecc19547298b7f110419c84
ShaneRer1 <shane53723@gmail.com>
2025-09-19 20:19:52 +1000

    added category total to total expenses option

--- Tracker.py
+++ Tracker.py
@@ -99,6 +99,27 @@ def todays_expenses():
     todays_expenses = [expense for expense in expenses if datetime.strptime(expense['date'], "%d-%m-%Y %H:%M:%S").date() == today]
     return todays_expenses
 
+def category_expenses():
+    category_totals = {}
+    for expense in expenses:
+        category = expense['category']
+        amount = expense['amount']
+        if category in category_totals:
+            category_totals[category] += amount
+        else:
+            category_totals[category] = amount
+    
+    print(f"{'Category':<20} | {'Total Amount':<15}")
+    print("-" * 40)
+    for category, total in category_totals.items():
+        print(f"{category:<20} | ${total:<15.2f}")
+
+    print("-" * 40)
+    total_expenses = sum(category_totals.values())
+    print(f"{'Total':<20} | ${total_expenses:<15.2f}")
+    input("\nPress Enter to continue...")
+
+
 # Daily Expense Tracker, refreshes each day to 0 expenses function
 while True:
     expenses = load_expenses()
@@ -149,9 +170,7 @@ while True:
 
     elif choice == '3': #Total expenses
         clear_console()
-        total = sum(expense['amount'] for expense in expenses)
-        print(f"Total Expenses: ${total}")
-        input("\nPress Enter to continue...")
+        category_expenses()
 
     elif choice == '4': #View expense by category
         clear_console()
